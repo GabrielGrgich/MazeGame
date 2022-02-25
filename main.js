bcad7ea5eed0af54cb1c34f843be8c586198f112
TheIZDoggie27 <isaakthompson27>
2022-02-25 15:08:44 -0600

    this is a test run, if you see this, it worked!

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+//Seeing if this works. Anything between comments can be scraped
+let div1 = document.createElement(`div`)
+div1.style.width = `30px`
+div1.style.height = `30px`
+div1.style.border = `2px solid aquamarine`
+// Anything above can be scrapped

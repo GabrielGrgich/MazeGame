8f0d5a19589f274abe7a1a3bfd9396f903dbd6db
TheIZDoggie27 <isaakthompson27>
2022-02-26 11:16:47 -0600

    running tests.

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 //Seeing if this works. Anything between comments can be scraped
+let button1 = document.getElementById(`button1`)
+button1.addEventListener(`click`, color)
+
+function color() {
+    this.style.backgroundColor = `blue`
+}
+
 let div1 = document.createElement(`div`)
 div1.style.width = `30px`
 div1.style.height = `30px`

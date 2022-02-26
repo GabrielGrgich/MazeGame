b23eed4ad019a95090a279e6fea7fc077a8435dc
Gabe's Mac <gabegrgich@gmail.com>
2022-02-26 15:52:08 -0800

    test

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 //Seeing if this works. Anything between comments can be scraped
-let parentDiv = document.getElementById(`parentDiv`)
+
 
 let div1 = document.createElement(`div`)
-parentDiv.appendChild(div1)
 div1.style.width = `30px`
 div1.style.height = `30px`
-div1.style.border = `2px solid blue`
-div1.style.backgroundColor = `yellow`
-div1.style.borderRadius = `50%`
+div1.style.border = `2px solid aquamarine`
 // Anything above can be scrapped
-// test
+// testing testing 1 2 3

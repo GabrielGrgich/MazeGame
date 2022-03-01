d8cf97912ca542b08beb83a883155f34fd7c22ac
TheIZDoggie27 <isaakthompson27>
2022-03-01 15:24:20 -0600

    Running test to see how easily elements/divs can be created in main.js.

--- main.js
+++ main.js
@@ -4,5 +4,7 @@
 let div1 = document.createElement(`div`)
 div1.style.width = `30px`
 div1.style.height = `30px`
-div1.style.border = `2px solid aquamarine`
+div1.style.border = `2px solid blue`
+div1.style.backgroundColor = `yellow`
+div1.style.borderRadius = `50%`
 // Anything above can be scrapped
